b41f558d3ac453e9e8df3b1c513a47b37fdfc9bb
Djalminhagod <r.senra@liceolapaz.net>
2025-09-25 11:35:24 +0200

    .

--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -1,5 +1,18 @@
 package ejercicio1;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class principal {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String nombre="willy";
+        System.out.println( "el cerdo se llama "+ nombre);
+        System.out.println("inserta nombre" );
+        Scanner teclado = new Scanner(System.in);
+        nombre = teclado.nextLine();
+        System.out.println("el cerdo se llama "+ nombre);
+
+
+    }
+
 }
