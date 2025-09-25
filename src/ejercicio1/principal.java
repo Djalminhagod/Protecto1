ee2734d8960649e3e7e05e5667210230562c87cf
Djalminhagod <r.senra@liceolapaz.net>
2025-09-25 12:26:11 +0200

    .

--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 
 public class principal {
     public static void main(String[] args) {
-        String nombre="willy";
-        System.out.println( "el cerdo se llama "+ nombre);
-        System.out.println("inserta nombre" );
-        Scanner teclado = new Scanner(System.in);
-        nombre = teclado.nextLine();
-        System.out.println("el cerdo se llama "+ nombre);
-
+    Scanner Teclado = new Scanner(System.in);
+    System.out.println("Ingrese el Primer numero: ");
+    int numero = Teclado.nextInt();
+    System.out.println("Ingrese el Segundo numero: ");
+    int numero2 = Teclado.nextInt();
+    int resulsuma = numero + numero2;
+    System.out.println("El resultado es suma: " + resulsuma);
+    int resulresta = numero - numero2;
+    System.out.println("El resultado es resta: " + resulresta);
+    int resulmulti = numero * numero2;
+    System.out.println("El resultado es multi: " + resulmulti);
+    double resuldiv = (double) numero / numero2;
+    System.out.println("El resultado es div: " + resuldiv);
 
     }
 
