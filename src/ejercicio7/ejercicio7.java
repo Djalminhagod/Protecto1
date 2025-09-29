703983a6a26d47cab22c611d17654d9f9470c263
Djalminhagod <r.senra@liceolapaz.net>
2025-09-29 12:26:20 +0200

    ejercicio7 falta corregir

---
+++ src/ejercicio7/ejercicio7.java
@@ -0,0 +1,23 @@
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class ejercicio7 {
+    public static void main(String[] args) {
+        System.out.println("aplicacion iniciada");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nota del 0-10: ");
+        double nota = sc.nextDouble();
+        if (nota<=4) {
+            System.out.println("Suspenso ");
+        }else if (nota==5) {
+            System.out.println("Aprovado ");
+        }else if (nota==6) {
+            System.out.println("Bien ");
+        }else if (nota>=7 && nota>9) {
+            System.out.println("Notable ");
+        }else if (nota>=9 && nota<=10) {
+            System.out.println("Sobresaliente  ");
+        }
+    }
+}
