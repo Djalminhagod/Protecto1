5740b4667ba9d672b0d70dfc624a918179879564
Djalminhagod <r.senra@liceolapaz.net>
2025-09-30 13:28:45 +0200

    correccion 7

--- src/ejercicio7/ejercicio7.java
+++ src/ejercicio7/ejercicio7.java
@@ -8,16 +8,20 @@ public class ejercicio7 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nota del 0-10: ");
         double nota = sc.nextDouble();
-        if (nota<=4) {
-            System.out.println("Suspenso ");
-        }else if (nota==5) {
-            System.out.println("Aprovado ");
-        }else if (nota==6) {
-            System.out.println("Bien ");
-        }else if (nota>=7 && nota<9) {
-            System.out.println("Notable ");
-        }else if (nota>=9 && nota<=10) {
-            System.out.println("Sobresaliente  ");
+        if (!(nota>=0 && nota<=10)) {
+            System.out.println("La nota debe ser entre 0 e 10 ");
+        }else  {
+            if(nota<=4){
+                System.out.println("suspenso");
+            } else if (nota>=4 && nota<6) {
+                System.out.println("aprobado");
+            }else if (nota<7) {
+                System.out.println("bien");
+            }else if (nota<9) {
+                System.out.println("notable");
+            }else if (nota<=10) {
+                System.out.println("Sobresaliente");
+            }
         }
     }
 }
